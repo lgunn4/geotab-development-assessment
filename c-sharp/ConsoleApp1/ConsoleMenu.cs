6a1e190658a164da70076567b68abac802c74d94
Logan Gunn <lgunn4@uwo.ca>
2021-02-19 23:12:01 -0500

    Upgraded ConsoleMenu class and implemented program with new APIService class and Joke class

--- c-sharp/ConsoleApp1/ConsoleMenu.cs
+++ c-sharp/ConsoleApp1/ConsoleMenu.cs
@@ -5,11 +5,56 @@ namespace JokeGenerator
 {
     public static class ConsoleMenu
     {
-        public static string PrintOption(string displayText)
-        { 
+        private static ConsoleColor _inputTextColor = Console.ForegroundColor;
+        private static ConsoleColor _promptTextColor = Console.ForegroundColor;
+        private static ConsoleColor _informationTextColor = Console.ForegroundColor;
+        
+        public static void setInputTextColor(ConsoleColor inputTextColor)
+        {
+            _inputTextColor = inputTextColor;
+        }
+
+        public static void setPromptTextColor(ConsoleColor promptTextColor)
+        {
+            _promptTextColor = promptTextColor;
+        }
+        
+        public static void setInformationTextColor(ConsoleColor informationTextColor)
+        {
+            _informationTextColor = informationTextColor;
+        }
+        public static void Print(string displayText, ConsoleColor foregroundColor)
+        {
+            var originalColor = _inputTextColor;
+            Console.ForegroundColor = foregroundColor;
             Console.WriteLine(displayText);
-            var enteredValue =  Console.ReadKey().Key.ToString();
-            Console.WriteLine(Environment.NewLine);
+            Console.ForegroundColor = originalColor;
+        }
+        
+        public static void PrintInformation(string displayText)
+        {
+            Print(displayText, _informationTextColor);
+        }
+        
+        public static void PrintPrompt(string displayText)
+        {
+            Print(displayText, _promptTextColor);
+        }
+
+        private static string PrintOption(string displayText)
+        {
+            PrintPrompt(displayText);
+            var enteredValue = Console.ReadKey().Key.ToString();
+            EmptyLine();
+
+            return enteredValue;
+        }
+        
+        public static string PrintOptionInput(string displayText)
+        { 
+            PrintPrompt(displayText);
+            var enteredValue = Console.ReadLine();
+            EmptyLine();
 
             return enteredValue;
         }
@@ -19,6 +64,22 @@ namespace JokeGenerator
             while (true)
             {
                 var enteredInput = PrintOption(displayText);
+                if (options.Contains(enteredInput))
+                {
+                    return enteredInput;
+                }
+            }
+        }
+        
+        public static string PrintOptionsInput(string displayText, string[] options)
+        {
+            while (true)
+            {
+                var enteredInput = PrintOptionInput(displayText);
+                if (string.Empty.Equals(enteredInput))
+                {
+                    return enteredInput;
+                }
                 if (options.Contains(enteredInput))
                 {
                     return enteredInput;
@@ -37,9 +98,9 @@ namespace JokeGenerator
         {
             while (true)
             {
-                Console.WriteLine(displayText);
+                PrintPrompt(displayText);
                 var enteredInput = GetEnteredInt(Console.ReadKey().KeyChar);
-                Console.WriteLine(Environment.NewLine);
+                EmptyLine();
                 if (enteredInput > 0)
                 {
                     return enteredInput;
@@ -55,6 +116,11 @@ namespace JokeGenerator
             }
 
             return -1;
+        }
+
+        public static void EmptyLine()
+        {
+            Console.WriteLine(Environment.NewLine);
         }
     }
 }
--- c-sharp/ConsoleApp1/Program.cs
+++ c-sharp/ConsoleApp1/Program.cs
@@ -1,83 +1,89 @@
 ﻿using System;
-using System.Collections;
-using System.Linq;
-using System.Xml;
+using Newtonsoft.Json;
 
 namespace JokeGenerator
 {
-    class Program
+    static class Program
     {
-        static string[] results = new string[50];
-        static Tuple<string, string> names;
+        private static string[] categories;
 
         private const string CategoryOption = "C";
         private const string RandomOption = "R";
         private const string QuitOption = "Q";
+        private static readonly string[] MainMenuOptions = {CategoryOption, RandomOption, QuitOption};
 
-        private static readonly string[] MainMenuOptions = new[] {CategoryOption, RandomOption, QuitOption};
+        private const ConsoleColor InputTextColor = ConsoleColor.White;
+        private const ConsoleColor PromptTextColor = ConsoleColor.Red;
+        private const ConsoleColor InformationTextColor = ConsoleColor.Yellow;
 
-        //TODO: Initialize this arrayList when the program starts to all categories in the API
-        private static ArrayList categories = new ArrayList();
-
-        public static void Main(string[] args) // needed to be public 
+        
+        public static void Main(string[] args)
         {
-            if (ConsoleMenu.PrintOption("Press ? to get instructions.").Equals("?")) return;  // bug here needed .equals
+            InitializeCategories();
+            ConsoleMenu.setInputTextColor(InputTextColor);
+            ConsoleMenu.setPromptTextColor(PromptTextColor);
+            ConsoleMenu.setInformationTextColor(InformationTextColor);
 
             var mainMenuOption = "";
-            while (!mainMenuOption.Equals(QuitOption)) //infinite loop in original code assume needs quit command.
+            while (!mainMenuOption.Equals(QuitOption))
             {
-                mainMenuOption = ConsoleMenu.PrintOptions("Press c to get categories, Press r to get random jokes, Press q to quit", MainMenuOptions);
-
                 switch (mainMenuOption)
                 {
                     case CategoryOption:
-                        Console.WriteLine(categories.ToArray().ToString());
-                        break;
-                    case RandomOption:
-                    {
-                        if (ConsoleMenu.PrintBoolOption("Want to use a random name? y/n"))
-                            GetNames();
-
-                        var category = "";
-                        var numberOfJokes = ConsoleMenu.PrintIntOption("How many jokes do you want? (1-9)");
-                        if (ConsoleMenu.PrintBoolOption("Want to specify a category? y/n"))
+                        var categoryList = "Categories: ";
+                        foreach (var category in categories)
                         {
-                            category = ConsoleMenu.PrintOptions("Enter a category", categories.ToArray() as string[]);
+                            categoryList = categoryList + category + ", ";
                         }
 
-                        GetRandomJokes(category, numberOfJokes);
-                        PrintResults();
+                        categoryList = categoryList.Substring(0, categoryList.Length - 2);
+                        ConsoleMenu.PrintInformation(categoryList);
+                        break;
+                    
+                    case RandomOption:
+                    {
+                        Joke joke;
+                        if (ConsoleMenu.PrintBoolOption("Want to use a random name? y/n"))
+                        {
+                            joke = new Joke();
+                        }
+                        else
+                        {
+                            var firstName = ConsoleMenu.PrintOptionInput("Enter a first Name");
+                            var lastName = ConsoleMenu.PrintOptionInput("Enter a last Name");
+                            
+                            joke = new Joke(firstName, lastName);
+                        }
+                        var numberOfJokes = ConsoleMenu.PrintIntOption("How many jokes do you want? (1-9)");
+                        
+                        if (ConsoleMenu.PrintBoolOption("Want to specify a category? y/n"))
+                        {
+                            string category = ConsoleMenu.PrintOptionsInput("Enter a valid category or hit enter to skip", categories);
 
+                            if (!string.Empty.Equals(category))
+                            {
+                                joke.SetCategory(category);
+                            }
+                        }
+
+                        for (var i = 0; i < numberOfJokes; i++)
+                        {
+                            ConsoleMenu.PrintInformation(joke.GetJoke());
+                        }
+
+                        ConsoleMenu.EmptyLine();
                         break;
                     }
                 }
+                mainMenuOption = ConsoleMenu.PrintOptions("Press c to get categories, Press r to get random jokes, Press q to quit", MainMenuOptions);
             }
         }
 
-        private static void PrintResults()
+        private static void InitializeCategories()
         {
-            Console.WriteLine("[" + string.Join(",", results) + "]");
-        }
-
-       
-
-        private static void GetRandomJokes(string category, int number)
-        {
-            new JsonFeed("https://api.chucknorris.io", number);
-            results = JsonFeed.GetRandomJokes(names?.Item1, names?.Item2, category);
-        }
-
-        private static void getCategories()
-        {
-            new JsonFeed("https://api.chucknorris.io", 0);
-            results = JsonFeed.GetCategories();
-        }
-
-        private static void GetNames()
-        {
-            new JsonFeed("https://www.names.privserv.com/api/", 0);
-            dynamic result = JsonFeed.Getnames();
-            names = Tuple.Create(result.name.ToString(), result.surname.ToString());
+            ApiService categoryApiService = new ApiService("https://api.chucknorris.io/jokes/categories");
+            categories = JsonConvert.DeserializeObject<string[]>(categoryApiService.GetRequest());
+            
         }
     }
 }
