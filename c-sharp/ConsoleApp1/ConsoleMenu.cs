aec4ccb8a09fb15dcb7155a8ad6bafd38b7e7e3f
Logan Gunn <lgunn4@uwo.ca>
2021-02-19 20:29:31 -0500

    Changed ConsolePrinter to a static ConsoleMenu class

---
+++ c-sharp/ConsoleApp1/ConsoleMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace JokeGenerator
+{
+    public static class ConsoleMenu
+    {
+        public static string PrintOption(string value)
+        { 
+            Console.WriteLine(value);
+            var enteredValue =  Console.ReadKey().Key.ToString();
+            Console.WriteLine(Environment.NewLine);
+
+            return enteredValue;
+        }
+
+        public static string PrintOptions(string value, string[] options)
+        {
+            while (true)
+            {
+                var enteredInput = PrintOption(value);
+                if (options.Contains(enteredInput))
+                {
+                    return enteredInput;
+                }
+            }
+        }
+
+        public static bool PrintBoolOption(string value)
+        {
+            var option = PrintOptions(value, new[] {"Y", "N"});
+
+            return option.Equals("Y");
+        }
+
+        public static int PrintIntOption(string value)
+        {
+            while (true)
+            {
+                var enteredInput = GetEnteredInt(PrintOption(value));
+                if (enteredInput > 0)
+                {
+                    return enteredInput;
+                }
+            }
+        }
+
+        private static int GetEnteredInt(string enteredInput)
+        {
+            if (char.IsDigit(char.Parse(enteredInput)))
+            {
+                return int.Parse(enteredInput);
+            }
+
+            return -1;
+        }
+    }
+}
--- c-sharp/ConsoleApp1/Program.cs
+++ c-sharp/ConsoleApp1/Program.cs
@@ -1,111 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Xml;
 
-namespace ConsoleApp1
+namespace JokeGenerator
 {
     class Program
     {
         static string[] results = new string[50];
-        static char key;
         static Tuple<string, string> names;
-        static ConsolePrinter printer = new ConsolePrinter();
 
-        static void Main(string[] args)
+        private const string CategoryOption = "C";
+        private const string RandomOption = "R";
+        private const string QuitOption = "Q";
+
+        private static readonly string[] MainMenuOptions = new[] {CategoryOption, RandomOption, QuitOption};
+
+        //TODO: Initialize this arrayList when the program starts to all categories in the API
+        private static ArrayList categories = new ArrayList();
+
+        public static void Main(string[] args) // needed to be public 
         {
-            printer.Value("Press ? to get instructions.").ToString();
-            if (Console.ReadLine() == "?")
+            if (ConsoleMenu.PrintOption("Press ? to get instructions.").Equals("?")) return;  // bug here needed .equals
+
+            var mainMenuOption = "";
+            while (!mainMenuOption.Equals(QuitOption)) //infinite loop in original code assume needs quit command.
             {
-                while (true)
+                mainMenuOption = ConsoleMenu.PrintOptions("Press c to get categories, Press r to get random jokes, Press q to quit", MainMenuOptions);
+
+                switch (mainMenuOption)
                 {
-                    printer.Value("Press c to get categories").ToString();
-                    printer.Value("Press r to get random jokes").ToString();
-                    GetEnteredKey(Console.ReadKey());
-                    if (key == 'c')
+                    case CategoryOption:
+                        Console.WriteLine(categories.ToArray().ToString());
+                        break;
+                    case RandomOption:
                     {
-                        getCategories();
-                        PrintResults();
-                    }
-                    if (key == 'r')
-                    {
-                        printer.Value("Want to use a random name? y/n").ToString();
-                        GetEnteredKey(Console.ReadKey());
-                        if (key == 'y')
+                        if (ConsoleMenu.PrintBoolOption("Want to use a random name? y/n"))
                             GetNames();
-                        printer.Value("Want to specify a category? y/n").ToString();
-                        if (key == 'y')
+
+                        var category = "";
+                        var numberOfJokes = ConsoleMenu.PrintIntOption("How many jokes do you want? (1-9)");
+                        if (ConsoleMenu.PrintBoolOption("Want to specify a category? y/n"))
                         {
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-                            printer.Value("Enter a category;").ToString();
-                            GetRandomJokes(Console.ReadLine(), n);
-                            PrintResults();
+                            category = ConsoleMenu.PrintOptions("Enter a category", categories.ToArray() as string[]);
                         }
-                        else
-                        {
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-                            GetRandomJokes(null, n);
-                            PrintResults();
-                        }
+
+                        GetRandomJokes(category, numberOfJokes);
+                        PrintResults();
+
+                        break;
                     }
-                    names = null;
                 }
             }
-
         }
 
         private static void PrintResults()
         {
-            printer.Value("[" + string.Join(",", results) + "]").ToString();
+            Console.WriteLine("[" + string.Join(",", results) + "]");
         }
 
-        private static void GetEnteredKey(ConsoleKeyInfo consoleKeyInfo)
-        {
-            switch (consoleKeyInfo.Key)
-            {
-                case ConsoleKey.C:
-                    key = 'c';
-                    break;
-                case ConsoleKey.D0:
-                    key = '0';
-                    break;
-                case ConsoleKey.D1:
-                    key = '1';
-                    break;
-                case ConsoleKey.D3:
-                    key = '3';
-                    break;
-                case ConsoleKey.D4:
-                    key = '4';
-                    break;
-                case ConsoleKey.D5:
-                    key = '5';
-                    break;
-                case ConsoleKey.D6:
-                    key = '6';
-                    break;
-                case ConsoleKey.D7:
-                    key = '7';
-                    break;
-                case ConsoleKey.D8:
-                    key = '8';
-                    break;
-                case ConsoleKey.D9:
-                    key = '9';
-                    break;
-                case ConsoleKey.R:
-                    key = 'r';
-                    break;
-                case ConsoleKey.Y:
-                    key = 'y';
-                    break;
-            }
-        }
+       
 
         private static void GetRandomJokes(string category, int number)
         {
