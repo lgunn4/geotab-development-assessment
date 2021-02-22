7c1ed9fe21b8ead27d04f4974ba7caabbf26ad37
Logan Gunn <lgunn4@uwo.ca>
2021-02-21 23:37:45 -0500

    Added Config for ConsoleMenu and test for config

--- c-sharp/ConsoleApp1/Config/ConsoleMenuConfig.cs
+++ c-sharp/ConsoleApp1/Config/ConsoleMenuConfig.cs
@@ -6,12 +6,17 @@ namespace JokeGenerator.Config
 {
     public class ConsoleMenuConfig : IConsoleMenuConfig
     {
+        private const ConsoleColor DefaultColor = ConsoleColor.White;
         public ConsoleColor InputTextColor { get; }
         public ConsoleColor PromptTextColor { get; }
         public ConsoleColor InformationTextColor { get; }
 
         public ConsoleMenuConfig(IConfiguration config)
         {
+            InputTextColor = DefaultColor;
+            PromptTextColor = DefaultColor;
+            InformationTextColor = DefaultColor;
+            
             var tempInputColor = config.GetValue<string>("ConsoleColor:Input");
             var tempPromptColor = config.GetValue<string>("ConsoleColor:Prompt");
             var tempInformationColor = config.GetValue<string>("ConsoleColor:Information");
@@ -20,14 +25,21 @@ namespace JokeGenerator.Config
             {
                 InputTextColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), tempInputColor);
             }
+
             if (Enum.TryParse(tempPromptColor, out ConsoleColor _))
             {
                 PromptTextColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), tempPromptColor);
             }
+
             if (Enum.TryParse(tempInformationColor, out ConsoleColor _))
             {
                 InformationTextColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), tempInformationColor);
             }
+        }
+
+        public ConsoleColor GetDefaultColor()
+        {
+            return DefaultColor;
         }
     }
 }
